abba47202442ba28ef5080cd3e2fcf9f23d93151
ottermegazord <idaly.ali@live.fr>
2017-09-17 14:21:00 -0400

    Graphs for all three systems

--- PS2/Q2.py
+++ PS2/Q2.py
@@ -22,21 +22,25 @@ N_0 = 20.
 ld_l = 0
 ld_r = 0
 W_b = np.linspace(0,20., 21)
+W_c = np.linspace(0,20., 21)
+
+#C
+T_0 = 10.
 
 print T
 
-# mean_timeA = (T/2) - pert
-# plt.plot(x_range, mean_timeA)
-# plt.title('Plot of mean waiting time against number of pedestrians with constant P')
-# plt.xlabel("Number of Pedestrians / minute")
-# plt.ylabel("Mean waiting time / minute""Number of Pedestrians / minute")
-# plt.show()
+mean_timeA = (T/2) - pert
+plt.plot(x_range, mean_timeA)
+plt.title('Plot of Perturbation time against waiting time (System A)')
+plt.xlabel("Perturbation / minute")
+plt.ylabel("Waiting time / minute")
+plt.show()
 
 for i in range(2):
     ld_r = ld_r + 0.1
     for j in range(2):
         ld_l = ld_l + 0.1
-        P = ((N_0 - 1.) / (2.*(ld_l + ld_r))) - W_b
+        P = ((N_0 - 1.) / (2. * (ld_l + ld_r))) - W_b
         #plt.legend(ld_l, ld_r)
         plt.plot(W_b, P, label='ld_l: %.1f, ld_r: %.1f' % (ld_l, ld_r))
 plt.legend(loc='best')
@@ -46,4 +50,20 @@ plt.ylabel("Perturbation / minute")
 plt.xlabel("Waiting time / minute")
 plt.show()
 
+ld_l = 0
+ld_r = 0
 
+for i in range(2):
+    ld_r = ld_r + 0.1
+    for j in range(2):
+        ld_l = ld_l + 0.1
+        P = (T_0 / 2) * (1 + (1 / (1 + T_0 * (ld_l + ld_l)))) - W_c
+        #plt.legend(ld_l, ld_r)
+        plt.plot(W_b, P, label='ld_l: %.1f, ld_r: %.1f' % (ld_l, ld_r))
+
+plt.legend(loc='best')
+plt.title('Plot of Perturbation time against waiting time (System C)')
+plt.ylabel("Perturbation / minute")
+#plt.ylim([0 , 30])
+plt.xlabel("Waiting time / minute")
+plt.show()
